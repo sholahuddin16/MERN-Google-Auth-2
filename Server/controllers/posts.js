05ceb612b32c1b6da1ab11993423a7f0c96fa1d1
DESKTOP-6AMFINR\Muhammad Sholahuddin <msholahuddin16@gmail.com>
2022-02-03 01:41:53 +0700

    malam

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -19,16 +19,16 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost = new PostMessage(post);
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
 
     try {
-        await newPost.save();
+        await newPostMessage.save();
 
-        res.status(201).json(newPost);
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
-}
+};
 
 export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
